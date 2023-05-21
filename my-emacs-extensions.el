9b2301880ba8cae91c59b68e2c7eb531b18ac88f
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2023-05-21 12:51:22 +0000

    added autoloader file

---
+++ my-emacs-extensions.el
@@ -0,0 +1,39 @@
+;;; my-emacs-extensions.el --- Intro -*- lexical-binding: t -*-
+
+;; Copyright 2023 Ramus Jabee Lloyd Wilson
+
+;; Author: Ramus Jabee Lloyd Wilson
+;; Maintainer: Ramus Jabee Lloyd Wilson
+;; Created: 21 May 2023
+
+;; Keywords: keywords
+;; Homepage: "https://github.com/rwilson-lib/my-emacs-extensions"
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25.1") (package "0.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Base package proving custom extension to Emacs packages
+;;
+;; Exported names start with "my-emacs-extensions-"; private names start with "my-emacs-extensions--".
+
+;;; Code:
+
+
+(provide 'my-emacs-extensions)
+;;; my-emacs-extensions.el ends here
